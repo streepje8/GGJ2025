f3f290a4171021be8b6b3545a7d3dca618d3220d
streepje8 <wessel@wezzel.nl>
2025-01-26 12:50:43 +0100

    Draaimolen af

--- Assets/Scripts/FondantMetStokjes/InteractionSystem/Draaimolen.cs
+++ Assets/Scripts/FondantMetStokjes/InteractionSystem/Draaimolen.cs
@@ -6,20 +6,22 @@ public class Draaimolen : Interactable
     public MeshRenderer displayModel;
     public BubbleKind spinnedKind;
     public Transform bone;
+    public Transform boneMirror;
     public float spinningTime = 2f;
     private float turningCooldown = 0.2f;
     private float totalTimer = 0f;
     private float turningSpeed = 600f;
     private MeshRenderer display;
     private LiveBubble bubble;
-
+    
     public override void OnInteract(Interactor interactor)
     {
         if (!interactor.TryTakeBubble(out bubble)) return;
         StartInteraction(interactor);
         turningCooldown = 0.2f;
         totalTimer = 0f;
-        display = Instantiate(displayModel);
+        display = Instantiate(displayModel, boneMirror);
+        display.transform.localPosition = new Vector3(-0.29f, 0.553f, -0.53f);
         bubble.ToRenderer(display);
     }
 
@@ -37,14 +39,14 @@ public class Draaimolen : Interactable
             lastJoystick = input;
         }
         
-        display.transform.position = bone.transform.position + new Vector3(-0.272000015f,-0.294999987f-0.504000008f);
-        
         if (turningCooldown > 0)
         {
             turningCooldown -= Time.deltaTime;
             bone.localRotation *= Quaternion.Euler(0, turningSpeed * Time.deltaTime, 0);
             totalTimer += Time.deltaTime;
         }
+
+        boneMirror.localRotation = Quaternion.Euler(0, bone.rotation.eulerAngles.y, 0);
 
         if (totalTimer >= spinningTime)
         {
